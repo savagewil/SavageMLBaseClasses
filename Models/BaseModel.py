7449df6ebfea8978bdf0b12cdfe2107ee1b35757
wsavage <wsavage@hubspot.com>
2021-06-29 09:53:05 -0400

    Filled BaseModel.pyodel

--- Models/BaseModel.py
+++ Models/BaseModel.py
@@ -1,3 +1,21 @@
 from sklearn.base import BaseEstimator
-class BaseModel():
-    pass
+
+
+class BaseModel(BaseEstimator):
+    def __init__(self):
+        pass
+
+    def set_params(self, **params):
+        pass
+
+    def get_params(self, deep=True):
+        pass
+
+    def clone(self):
+        pass
+
+    def fit(self, x, y=None):
+        pass
+
+    def predict(self, x):
+        pass

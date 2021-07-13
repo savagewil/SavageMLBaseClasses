7f6739f039cd47d0b631987c7f5dff4253c9d8c7
wsavage <wsavage@hubspot.com>
2021-07-13 15:18:44 -0400

    fixing iteration of simulation

--- Tests/test_BaseSimulation.py
+++ Tests/test_BaseSimulation.py
@@ -81,3 +81,11 @@ def test_reset_random():
     simulation.reset()
     rngVal_reset = simulation.random.random()
     assert rngVal_first == rngVal_reset
+
+
+def test_simulation_iterates():
+    simulation = BaseSimulation()
+    inside_loop_ran = False
+    for _ in simulation:
+        inside_loop_ran = True
+    assert inside_loop_ran
--- simulations/base_simulation.py
+++ simulations/base_simulation.py
@@ -21,7 +21,11 @@ class BaseSimulation:
         return self.__class__(self.model, self.seed)
 
     def __next__(self):
-        return ()
+        if self.state == SimulationState.COMPLETE:
+            raise StopIteration
+        else:
+            result = self.step()
+            return result
 
     def set_seed(self, seed=get_default_seed()):
         self.seed = seed
@@ -39,8 +43,9 @@ class BaseSimulation:
     def get_state(self):
         return self.state
 
-    def step(self, visualize=False):
+    def step(self, visualize=False) -> tuple:
         self.state = SimulationState.COMPLETE
+        return ()
 
     def run(self, visualize=False):
         while not self.get_state() == SimulationState.COMPLETE:
